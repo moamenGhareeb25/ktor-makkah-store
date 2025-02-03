db7ca71eabffdaa30ba91a9569c257958e89abb2
moamenGhareeb25 <moamenghareeb25@gmail.com>
2025-02-03 17:24:38 +0200

    update Firebase

--- src/main/kotlin/com/example/firebase/Firebase.kt
+++ src/main/kotlin/com/example/firebase/Firebase.kt
@@ -10,16 +10,10 @@ object Firebase {
     fun init() {
         try {
             val firebaseBase64 = System.getenv("FIREBASE_CREDENTIALS")
-
-            if (firebaseBase64.isNullOrEmpty()) {
-                println("❌ FIREBASE_CREDENTIALS is missing! Firebase will NOT be initialized.")
-                return
-            }
+                ?: throw IllegalStateException("❌ FIREBASE_CREDENTIALS not set")
 
             // Decode Base64 to JSON
             val decodedJson = String(Base64.getDecoder().decode(firebaseBase64))
-
-            // Write the JSON to a temporary file (Render does not support in-memory Firebase credentials)
             val tempFile = File.createTempFile("firebase-admin", ".json").apply {
                 writeText(decodedJson)
             }
@@ -27,16 +21,13 @@ object Firebase {
             // Initialize Firebase
             val options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(tempFile.inputStream()))
-                .setStorageBucket(System.getenv("FIREBASE_STORAGE_BUCKET") ?: "makkah-store-operations.appspot.com") // Use env variable
                 .build()
 
-            synchronized(this) {
-                if (FirebaseApp.getApps().isEmpty()) {
-                    FirebaseApp.initializeApp(options)
-                    println("✅ Firebase initialized successfully!")
-                } else {
-                    println("✅ Firebase already initialized.")
-                }
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options)
+                println("✅ Firebase initialized successfully!")
+            } else {
+                println("✅ Firebase already initialized.")
             }
         } catch (e: Exception) {
             e.printStackTrace()
